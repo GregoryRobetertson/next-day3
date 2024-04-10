d73aef8c5f99d0ebedba432fe5dad30b27e4ab74
Gregory <geefromla@gmail.com>
2024-04-10 16:02:35 -0500

    update file

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    <div> {/* Wrapping elements within a single parent element */}
+    <div> 
       <section>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores tenetur necessitatibus aliquam explicabo, reprehenderit praesentium rem ab consequatur debitis eum, odio optio ut repudiandae veniam aperiam dolore ullam id accusantium. Quidem magnam quo eaque non maiores perspiciatis esse animi repellendus, placeat, id similique iusto explicabo quisquam eveniet consequuntur maxime soluta modi nemo vel rem. Beatae repellat modi et ex exercitationem?
